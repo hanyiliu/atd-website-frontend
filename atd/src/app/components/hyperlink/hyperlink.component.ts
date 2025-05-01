b6b00e1fe843e596983d6bcf0146164875524c3c
sanjith-iype <sanjithiype@gmail.com>
2025-05-01 12:16:58 -0700

    Test
    Commiting right now

---
+++ atd/src/app/components/hyperlink/hyperlink.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hyperlink',
+  imports: [],
+  templateUrl: './hyperlink.component.html',
+  styleUrl: './hyperlink.component.scss'
+})
+export class HyperlinkComponent {
+
+}
