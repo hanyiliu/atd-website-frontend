f5cf2334a844834ea4893c215e4167ab33545640
sanjith-iype <sanjithiype@gmail.com>
2025-05-09 13:59:43 -0700

    Primary Button Component
    Button Component for hero section

--- atd/src/app/components/button/button.component.ts
+++ atd/src/app/components/button/button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router'; // to navigate to different page when button clicked
 
 @Component({
   selector: 'app-button',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+  @Input() label: string = 'About Us';
+  @Input() route: string = '/about'; //routes page to about section of website
+  @Input() primary: boolean = false;
+  @Input() size: string = 'medium';
+  @Input() onClick: () => void = () => {} //dont fully understand this line
+  @Input() backgroundColor: string = '#000000';
 
+
+  constructor(private router: Router){} //Why do we need this?
+
+  navigate() {
+    this.router.navigate([this.route]);
+  }
 }
--- atd/src/app/components/button/button.stories.ts
+++ atd/src/app/components/button/button.stories.ts
@@ -1,12 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-import { fn } from '@storybook/test';
-
+import { action } from '@storybook/addon-actions';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ButtonComponent } from './button.component';
+import { moduleMetadata } from '@storybook/angular';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<ButtonComponent> = {
   title: 'Components/Button',
   component: ButtonComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [RouterTestingModule],
+    }),
+  ],
   tags: ['autodocs'],
   argTypes: {
     backgroundColor: {
@@ -14,23 +20,26 @@ const meta: Meta<ButtonComponent> = {
     },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  args: { onClick: action('clicked') },
 };
 
 export default meta;
-type Story = StoryObj<ButtonComponent>;
+type Story = StoryObj<ButtonComponent>; 
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button',
+    label: 'Primary Button',
+    backgroundColor: "#5f20a6",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    label: 'Second Button',
+    backgroundColor: '#DDDDDD',
+
   },
 };
 
