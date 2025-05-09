58c7408a47abee1abd19b47ad7a9c4569aca4821
Hanyi Liu <hanyiliu0@gmail.com>
2025-05-09 16:25:10 -0700

    feat: Fixed transition and applied correct padding

--- atd/src/app/components/faq-item/faq-item.component.stories.ts
+++ atd/src/app/components/faq-item/faq-item.component.stories.ts
@@ -18,6 +18,6 @@ type Story = StoryObj<FaqItemComponent>;
 export const Primary: Story = {
   args: {
     "question": "This is a sample question. Lorem ipsum dolor sit amet?",
-    "answer": "This is a sample answer response. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "answer": "This is a sample answer response. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
   },
 };
