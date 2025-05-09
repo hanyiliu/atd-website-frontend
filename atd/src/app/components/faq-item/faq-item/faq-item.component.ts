11a62dca4863290a8bd1655aa23fed4b2642bfad
Hanyi Liu <hanyiliu0@gmail.com>
2025-05-09 10:58:19 -0700

    feat: setup barebone faq-item

---
+++ atd/src/app/components/faq-item/faq-item/faq-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-item',
+  imports: [],
+  templateUrl: './faq-item.component.html',
+  styleUrl: './faq-item.component.scss'
+})
+export class FaqItemComponent {
+
+}
