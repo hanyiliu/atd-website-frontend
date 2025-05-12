722bea955fa5e677837e7e0d362d432ddbad8367
Cody Chen <codychen8888@gmail.com>
2025-05-11 19:27:47 -0700

    Complete Calendar

---
+++ atd/src/app/components/event-calender-box/event-calender-box.stories.ts
@@ -0,0 +1,39 @@
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { EventCalenderBoxComponent } from './event-calender-box.component';
+
+const meta: Meta<EventCalenderBoxComponent> = {
+  title: 'Components/EventCalenderBox',
+  component: EventCalenderBoxComponent,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<EventCalenderBoxComponent>;
+
+export const Primary: Story = {
+  args: {
+    events: [
+      {
+        day: '22',
+        month: 'April',
+        title: 'Info Night',
+        time: 'TBD',
+        location: 'TBD',
+      },
+      {
+        day: '23',
+        month: 'April',
+        title: 'Design Challenge Workshop',
+        time: 'TBD',
+        location: 'TBD',
+      },
+      {
+        day: '24',
+        month: 'April',
+        title: 'Meet the Board',
+        time: 'TBD',
+        location: 'Frenzy Coffee',
+      },
+    ],
+  },
+};
