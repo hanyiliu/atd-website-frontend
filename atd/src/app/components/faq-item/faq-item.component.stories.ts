b11460751f380a5482b4887defe8ffa35297cd72
Hanyi Liu <hanyiliu0@gmail.com>
2025-05-13 16:34:29 -0700

    feat: Implemented FAQ section

--- atd/src/app/components/faq-item/faq-item.component.stories.ts
+++ atd/src/app/components/faq-item/faq-item.component.stories.ts
@@ -16,8 +16,10 @@ type Story = StoryObj<FaqItemComponent>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  args: {
-    "question": "This is a sample question. Lorem ipsum dolor sit amet?",
-    "answer": "This is a sample answer response. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+  args: { 
+    "faqItem": {
+      "question": "This is a sample question. Lorem ipsum dolor sit amet?",
+      "answer": "This is a sample answer response. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+    }
   },
 };
--- atd/src/app/components/faq-item/faq-item.component.ts
+++ atd/src/app/components/faq-item/faq-item.component.ts
@@ -1,5 +1,7 @@
-import { Component, input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { FaqItem } from './faq-item.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-faq-item',
@@ -8,8 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrls: ['./faq-item.component.scss'],
 })
 export class FaqItemComponent {
-  question = input.required<string>();
-  answer = input.required<string>();
+  @Input() faqItem!: FaqItem;
   isExpanded = false;
 
   toggle(): void {
