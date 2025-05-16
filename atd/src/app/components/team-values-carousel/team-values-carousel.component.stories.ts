b4497d085db183899f8f926bf8eba0cbd6a4b5dd
Hanyi Liu <hanyiliu0@gmail.com>
2025-05-16 13:11:21 -0700

    refactor: rename component to TeamValuesCarouselComponent
    - To avoid conflict with values section in About Us page.

--- atd/src/app/components/team-values-carousel/team-values-carousel.component.stories.ts
+++ atd/src/app/components/team-values-carousel/team-values-carousel.component.stories.ts
@@ -1,18 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 
-import { ValuesSectionComponent } from './values-section.component';
+import { TeamValuesCarouselComponent } from './team-values-carousel.component';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-const meta: Meta<ValuesSectionComponent> = {
-  title: 'Components/ValuesSection',
-  component: ValuesSectionComponent,
+const meta: Meta<TeamValuesCarouselComponent> = {
+  title: 'Components/TeamValuesCarousel',
+  component: TeamValuesCarouselComponent,
   tags: ['autodocs'],
   argTypes: {
   },
 };
 
 export default meta;
-type Story = StoryObj<ValuesSectionComponent>;
+type Story = StoryObj<TeamValuesCarouselComponent>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
