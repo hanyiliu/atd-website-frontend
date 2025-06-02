bce9735980de4ff61e232bf7419bd9ad5f6df784
sanjith-iype <sanjithiype@gmail.com>
2025-06-02 15:02:23 -0700

    Finished Team Component

--- atd/src/app/components/team-component/team-component.component.ts
+++ atd/src/app/components/team-component/team-component.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Member } from '../member-component/member.model';
+import { MemberComponentComponent } from '../member-component/member-component.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-team-component',
-  imports: [],
+  standalone: true,
+  imports: [MemberComponentComponent, CommonModule],
   templateUrl: './team-component.component.html',
-  styleUrl: './team-component.component.scss',
+  styleUrls: ['./team-component.component.scss'],
 })
 export class TeamComponentComponent {
   @Input() members: Member[] = [];
--- atd/src/app/components/team-component/team-component.stories.ts
+++ atd/src/app/components/team-component/team-component.stories.ts
@@ -12,10 +12,72 @@ type Story = StoryObj<TeamComponentComponent>;
 
 export const Primary: Story = {
   args: {
-    members: {
-      image: 'images/nicole.svg',
-      title: 'VP of Secretary',
-      name: 'Nicole Kim',
-    },
+    members: [
+      {
+        image: 'images/richie.svg',
+        title: 'Co-Founder / President',
+        name: 'Richie Huang',
+      },
+      {
+        image: 'images/giang.svg',
+        title: 'Co-Founder / President',
+        name: 'Giang Ta',
+      },
+      {
+        image: 'images/katie.svg',
+        title: 'VP of Operations',
+        name: 'Katie Castro',
+      },
+      {
+        image: 'images/ryan.svg',
+        title: 'VP of Creative Directory',
+        name: 'Ryan Song',
+      },
+      {
+        image: 'images/nikita.svg',
+        title: 'VP of Education',
+        name: 'Nikita Dutt',
+      },
+      {
+        image: 'images/nicole.svg',
+        title: 'VP of Secretary',
+        name: 'Nicole Kim',
+      },
+      {
+        image: 'images/trinh.svg',
+        title: 'VP of Treasury',
+        name: 'Trinh Nguyen',
+      },
+      {
+        image: 'images/naomi.svg',
+        title: 'Graphics Chair',
+        name: 'Naomi Solnick',
+      },
+      {
+        image: 'images/lauren.svg',
+        title: 'Graphics Chair',
+        name: 'Lauren Calderon',
+      },
+      {
+        image: 'images/reiley.svg',
+        title: 'Graphics Chair',
+        name: 'Reiley Johnson',
+      },
+      {
+        image: 'images/essie.svg',
+        title: 'Apparel Chair',
+        name: 'Essence (Essie) Bailey',
+      },
+      {
+        image: 'images/emily.svg',
+        title: 'UI/UX Design Chair',
+        name: 'Emily Kang',
+      },
+      {
+        image: 'images/sami.svg',
+        title: 'Digital Chair',
+        name: 'Sami Deshatty',
+      },
+    ],
   },
 };
