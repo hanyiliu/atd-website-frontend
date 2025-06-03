2185c56404a9baf1321c42dbe860007fafd4b4f4
Hanyi Liu <hanyiliu0@gmail.com>
2025-06-03 13:05:41 -0700

    feat: Implemented Join Us Header

---
+++ atd/src/app/components/join-us-header/join-us-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-us-header',
+  imports: [],
+  templateUrl: './join-us-header.component.html',
+  styleUrl: './join-us-header.component.scss'
+})
+export class JoinUsHeaderComponent {
+
+}
