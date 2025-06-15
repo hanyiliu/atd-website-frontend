e19c9efa701d0c3637e3837d2237208ecc625131
Hanyi Liu <hanyiliu0@gmail.com>
2025-06-14 18:46:51 -0700

    feat: Implemented router

---
+++ atd/src/app/pages/join-us-page/join-us-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-us-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './join-us-page.component.html',
+  styleUrl: './join-us-page.component.scss'
+})
+export class JoinUsPageComponent {
+
+}
