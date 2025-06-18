7f2bc3b45c7794a5339dce377591354f9f5174f8
Cody Chen <codychen8888@gmail.com>
2025-06-17 23:11:36 -0700

    storybook fixes

--- atd/src/app/pages/about-us-page/about-us-page.component.ts
+++ atd/src/app/pages/about-us-page/about-us-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AboutUsHeaderComponent } from '../../components/about-us-header/about-us-header.component';
 import { InfoSectionComponent } from '../../components/info-section/info-section.component';
 import { VideoSectionComponent } from '../../components/video-section/video-section.component';
+import { TeamValuesCarouselComponent } from '../../components/team-values-carousel/team-values-carousel.component';
+import { MemberComponentComponent } from '../../components/member-component/member-component.component';
 
 @Component({
   selector: 'app-about-us-page',
@@ -10,6 +12,7 @@ import { VideoSectionComponent } from '../../components/video-section/video-sect
     AboutUsHeaderComponent,
     InfoSectionComponent,
     VideoSectionComponent,
+    TeamValuesCarouselComponent,
   ],
   templateUrl: './about-us-page.component.html',
   styleUrl: './about-us-page.component.scss',
