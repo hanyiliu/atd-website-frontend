b67622430ea41acb97ad40a90c42b77a57c675cb
Cody Chen <106263294+codyc8@users.noreply.github.com>
2025-06-21 10:09:38 -0700

    Atd 33 about us page (#32)
    * Three components added. gotta wait for sanju's two componets to be merged
    
    * storybook fixes
    
    * Page completed
    
    Still need to work on sizing next
    
    ---------
    
    Co-authored-by: Hanyi Liu <35943468+hanyiliu@users.noreply.github.com>

--- atd/src/app/pages/about-us-page/about-us-page.component.ts
+++ atd/src/app/pages/about-us-page/about-us-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { AboutUsHeaderComponent } from '../../components/about-us-header/about-us-header.component';
+import { InfoSectionComponent } from '../../components/info-section/info-section.component';
+import { VideoSectionComponent } from '../../components/video-section/video-section.component';
+import { TeamComponentComponent } from '../../components/team-component/team-component.component';
+import { ValuesSectionComponent } from '../../components/values-section/values-section.component';
 
 @Component({
   selector: 'app-about-us-page',
   standalone: true,
-  imports: [],
+  imports: [
+    AboutUsHeaderComponent,
+    InfoSectionComponent,
+    VideoSectionComponent,
+    TeamComponentComponent,
+    ValuesSectionComponent,
+  ],
   templateUrl: './about-us-page.component.html',
-  styleUrl: './about-us-page.component.scss'
+  styleUrl: './about-us-page.component.scss',
 })
-export class AboutUsPageComponent {
-
-}
+export class AboutUsPageComponent {}
