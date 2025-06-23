034a1fa5acd67f4d9906b2e0d2af698797a90f6a
sanjith-iype <sanjithiype@gmail.com>
2025-06-23 15:57:45 -0700

    Transition
    fades in and scrolls to the top as needed

--- atd/src/app/app.component.ts
+++ atd/src/app/app.component.ts
@@ -1,14 +1,30 @@
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'atd';
+  fadeKey = 1;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const hasFragment = this.router.url.includes('#');
+        if (!hasFragment) {
+          this.fadeKey = 0;
+          setTimeout(() => (this.fadeKey = 1), 0);
+        }
+      });
+  }
 }
