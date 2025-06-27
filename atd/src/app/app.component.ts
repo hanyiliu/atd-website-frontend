058a9c7c03fd04e0d3990070aa2e9291c3d3d3c0
sanjith-iype <sanjithiype@gmail.com>
2025-06-27 10:36:49 -0700

    making the transition work
    have it where the new page fades in properly.

--- atd/src/app/app.component.ts
+++ atd/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -16,16 +17,24 @@ export class AppComponent {
   title = 'atd';
   fadeKey = 1;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe(() => {
+        this.fadeKey = 0;
+      });
+
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe(() => {
         const hasFragment = this.router.url.includes('#');
-        if (!hasFragment) {
+        if (!hasFragment && isPlatformBrowser(this.platformId)) {
           window.scrollTo(0, 0);
-          this.fadeKey = 0;
-          setTimeout(() => (this.fadeKey = 1), 0);
         }
+        setTimeout(() => (this.fadeKey = 1), 0);
       });
   }
 }
