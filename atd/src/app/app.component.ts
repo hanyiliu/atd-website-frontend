4d8c172a9f3a265c57e5dcf1ad15ea937036b6a9
sanjith-iype <sanjithiype@gmail.com>
2025-06-28 12:56:52 -0700

    almost fixed
    I got this to the point where there is a cross dissolve, but the only issue is that the old page scrolls to the top and fades out while the second one fades in. wil figure it out soon

--- atd/src/app/app.component.ts
+++ atd/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import {
+  Router,
+  NavigationEnd,
+  NavigationStart,
+  ActivatedRoute,
+} from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
@@ -6,35 +11,29 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { routeTransition } from './components/routeTransitions';
 
 @Component({
   selector: 'app-root',
   imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  animations: [routeTransition],
 })
 export class AppComponent {
   title = 'atd';
-  fadeKey = 1;
 
   constructor(
     private router: Router,
+    protected route: ActivatedRoute,
     @Inject(PLATFORM_ID) private platformId: Object
   ) {
     this.router.events
-      .pipe(filter((event) => event instanceof NavigationStart))
-      .subscribe(() => {
-        this.fadeKey = 0;
-      });
-
-    this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe(() => {
-        const hasFragment = this.router.url.includes('#');
-        if (!hasFragment && isPlatformBrowser(this.platformId)) {
+        if (isPlatformBrowser(this.platformId)) {
           window.scrollTo(0, 0);
         }
-        setTimeout(() => (this.fadeKey = 1), 0);
       });
   }
 }
---
+++ atd/src/app/components/routeTransitions.ts
@@ -0,0 +1,34 @@
+import {
+  animate,
+  style,
+  transition,
+  trigger,
+  query,
+  group,
+} from '@angular/animations';
+
+export const routeTransition = trigger('routeTransition', [
+  transition('* <=> *', [
+    query(
+      ':enter, :leave',
+      style({
+        position: 'fixed',
+        width: '100%',
+        top: '131px',
+        left: 0,
+        zIndex: 1,
+      }),
+      { optional: true }
+    ),
+    group([
+      query(':leave', [animate('500ms ease', style({ opacity: 0 }))], {
+        optional: true,
+      }),
+      query(
+        ':enter',
+        [style({ opacity: 0 }), animate('500ms ease', style({ opacity: 1 }))],
+        { optional: true }
+      ),
+    ]),
+  ]),
+]);
