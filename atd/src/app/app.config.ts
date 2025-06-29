07be25a0871efc4cab50fd33f38daf0ec9543993
Hanyi Liu <hanyiliu0@gmail.com>
2025-06-28 17:50:05 -0700

    feat: Resolve conflicts

--- atd/src/app/app.config.ts
+++ atd/src/app/app.config.ts
@@ -4,13 +4,8 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http'; // Import provideHttpClient
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
