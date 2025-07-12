602fd0cc8e5a975cec146d49581b47301dce7a94
Hanyi Liu <hanyiliu0@gmail.com>
2025-07-12 00:28:03 -0700

    feat: Moved all assets to appropriate folders

--- src/app/components/member-component/member-component.stories.ts
+++ src/app/components/member-component/member-component.stories.ts
@@ -13,7 +13,7 @@ type Story = StoryObj<MemberComponentComponent>;
 export const Primary: Story = {
   args: {
     member: {
-      image: 'images/nicole.svg',
+      image: 'members/secretary.svg',
       title: 'VP of Secretary',
       name: 'Nicole Kim',
     },
