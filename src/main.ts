6acaea10b40a1f0a3c4600e7298552a0a848d3bf
Cody Chen <codychen8888@gmail.com>
2025-07-12 16:40:04 -0700

    Update main.ts cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
-
-// bootstrapApplication(AppComponent, {
-//   providers: [
-//     // This provides routing and enables our custom scrolling
-//     provideRouter(
-//       routes,
-//       withInMemoryScrolling({
-//         anchorScrolling: 'enabled',
-//         scrollPositionRestoration: 'disabled',
-//       })
-//     ),
-//   ],
-// }).catch((err) => console.error(err));
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
