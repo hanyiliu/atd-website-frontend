4a8e80ffe215fa59e95aa513568cb7d8906cf5a8
Cody Chen <codychen8888@gmail.com>
2025-07-12 15:16:18 -0700

    navigates to the component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  /* ... */
-];
+// const routes: Routes = [
+//   /* ... */
+// ];
 
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      // These three options are all important
-      anchorScrolling: 'enabled',
-      scrollPositionRestoration: 'disabled', // <-- Set this to 'disabled' to give full control to our code
-      onSameUrlNavigation: 'reload',
-    }),
-  ],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+// @NgModule({
+//   imports: [
+//     RouterModule.forRoot(routes, {
+//       // These three options are all important
+//       anchorScrolling: 'enabled',
+//       scrollPositionRestoration: 'disabled',
+//       onSameUrlNavigation: 'reload',
+//     }),
+//   ],
+//   exports: [RouterModule],
+// })
+// export class AppRoutingModule {}
