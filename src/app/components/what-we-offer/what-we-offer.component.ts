aee90388428e8fdf827182d14d2ad165c4935279
Hanyi Liu <hanyiliu0@gmail.com>
2025-07-17 22:54:34 -0700

    feat: Create offer-item model

--- src/app/components/what-we-offer/what-we-offer.component.ts
+++ src/app/components/what-we-offer/what-we-offer.component.ts
@@ -2,11 +2,7 @@ import { Component, Input } from '@angular/core';
 import { OfferCardComponent } from '../offer-card/offer-card.component';
 import { ToggleButtonComponent } from '../toggle-button/toggle-button.component';
 import { CommonModule } from '@angular/common';
-
-export type OfferItem = {
-  title: string;
-  description: string;
-};
+import { OfferItem } from '../../models/offer-item.model';
 
 @Component({
   selector: 'app-what-we-offer',
---
+++ src/app/models/offer-item.model.ts
@@ -0,0 +1,4 @@
+export interface OfferItem {
+  title: string;
+  description: string;
+};
