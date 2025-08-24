1333086f2c71151b6766a098210be6f354ed3ffd
Cody Chen <codychen8888@gmail.com>
2025-08-24 16:04:08 -0700

    Images in contents.json

--- src/app/components/our-works/our-works.component.ts
+++ src/app/components/our-works/our-works.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data/data.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-our-works',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './our-works.component.html',
   styleUrl: './our-works.component.scss',
 })
-export class OurWorksComponent {}
+export class OurWorksComponent implements OnInit {
+  frame9: string = '';
+  frame10: string = '';
+  frame11: string = '';
+  frame12: string = '';
+  frame13: string = '';
+  frame14: string = '';
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    const content = this.dataService.getContent();
+    this.frame9 = content.frame9;
+    this.frame10 = content.frame10;
+    this.frame11 = content.frame11;
+    this.frame12 = content.frame12;
+    this.frame13 = content.frame13;
+    this.frame14 = content.frame14;
+  }
+}
--- src/app/models/content.model.ts
+++ src/app/models/content.model.ts
@@ -1,13 +1,21 @@
 export interface Content {
   // About Us page content
   videoUrl: string;
-  
-  // Contact 
+
+  // Contact
   contactEmail: string;
   contactSubject: string;
-  
+
   // Social media links
   instagramUrl: string;
   linkedinUrl: string;
   tiktokUrl: string;
+
+  //Our Works Images
+  frame9: string;
+  frame10: string;
+  frame11: string;
+  frame12: string;
+  frame13: string;
+  frame14: string;
 }
