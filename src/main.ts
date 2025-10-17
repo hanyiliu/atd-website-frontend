f1931b8ee6d9e06580c7dd9ce5a64b6ac19b8ce5
Hanyi Liu <hanyiliu0@gmail.com>
2025-10-17 13:12:52 -0700

    feat: Implement vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import 'hammerjs';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
